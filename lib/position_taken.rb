8dfadf8d3f2ed531ba61048e4aa4a755649cb911
Cory Pavitt <corypavitt@hotmail.com>
2018-07-12 10:42:00 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,4 +3,8 @@ def position_taken(index, user_input)
 
   if board[user_input] == " " || board[user_input] == "" || board[user_input] == nil
     return false
+  elsif board[user_input] == "X" ||  board [user_input == "O"]
+    return true
+  end
+
   end
